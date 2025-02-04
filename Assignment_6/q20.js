29aae22a339d9eb89b13d4c8f0a08b4b4c5ac3c2
Pranjal18004 <pranjal.mitra2023@vitstudent.ac.in>
2025-02-04 21:26:42 +0530

    Create q20.js

---
+++ Assignment_6/q20.js
@@ -0,0 +1,9 @@
+function countOccurrences(str) {
+    let charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    return charCount;
+}
+
+console.log(countOccurrences("hello"));
